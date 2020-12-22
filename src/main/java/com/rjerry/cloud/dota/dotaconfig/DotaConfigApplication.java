904f985f6a5c4d27f9981a2213850a8516d16565
Cozimetzer <jlouis.rafi@gmail.com>
2020-12-22 20:05:24 +0530

    Initial commit

---
+++ src/main/java/com/rjerry/cloud/dota/dotaconfig/DotaConfigApplication.java
@@ -0,0 +1,13 @@
+package com.rjerry.cloud.dota.dotaconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotaConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rjerry/cloud/dota/dotaconfig/DotaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rjerry.cloud.dota.dotaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotaConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
